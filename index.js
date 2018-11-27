1c3a13efa4023c7e2810ad42aacec05bc5ff5bc2
Ishai <ishai@bablic.com>
2018-11-27 09:38:50 +0200

    v2

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-
 function cleanName(name) {
-    return name.trim().replace(/^[:[\]\s]+/g, '').replace(/[:[\]\s]+$/g, '').replace(/[:[\]\s]+/g, '_');
+    return name.trim().replace(/^[:[\]\s]+/g, '').replace(/[:[\]\s]+$/g, '').replace(/[:[\]\s]+/g, '_').replace(/[^a-z_]/ig,'');
 }
 function run() {
-    //if(!process.argv[0].includes('kurdi')){
-    //    console.error('program must be kurdi');
-    //    return false;
-    //}
     const sourceFile = process.argv[2];
-    if(!sourceFile) {
+    if (!sourceFile) {
         console.error("no source file given");
         return false;
     }
     const targetFile = process.argv[3];
-    if(!targetFile){
+    if (!targetFile) {
         console.error("no target file given");
         return false;
     }
+    let sourceEncoding = process.argv[4] || 'utf8';
     const fs = require('fs');
     const _ = require('lodash');
-    const htmlStr = fs.readFileSync(sourceFile, 'utf8');
+    const decodedNeeded = !!sourceEncoding;
+    const inputBuffer = fs.readFileSync(sourceFile);
+    let htmlStr;
+    if (decodedNeeded) {
+        const iconv = require('iconv-lite');
+        htmlStr = iconv.decode(inputBuffer, sourceEncoding);
+    } else {
+        htmlStr = inputBuffer.toString('utf8');
+    }
 
     const $ = require("cheerio");
     const tables = $('table', htmlStr);
@@ -28,18 +32,18 @@ function run() {
         return false;
     }
     const output = {};
-    const first2 = tables.slice(0, 2);
+    const first2 = tables.slice(1, 2);
     _.each(first2, table => {
         const rows = $('tr', table);
         _.each(rows, row => {
             const cells = $('td', row);
-            if(cells.length != 2){
+            if (cells.length != 2) {
                 console.error('expected 2 cells in', row, 'skipping');
                 return;
             }
             const name = $(cells[0]).text();
             const value = $(cells[1]).text();
-            output[cleanName(name)] = value;
+            output[cleanName(name)] = value.trim();
 
         })
 
@@ -54,14 +58,14 @@ function run() {
         return cleanName($(cell).text());
     });
     const orders = _.map(otherRows, row => {
-       const cells = $('td', row);
-       const obj = {};
-       _.each(cells, (cell, i) => {
-           obj[columns[i]] = $(cell).text();
-       });
-       return obj;
+        const cells = $('td', row);
+        const obj = {};
+        _.each(cells, (cell, i) => {
+            obj[columns[i]] = $(cell).text();
+        });
+        return obj;
     });
-    output.orders = {order:orders};
+    output.orders = {order: orders};
 
     const xml2js = require('xml2js');
     const builder = new xml2js.Builder();
@@ -70,9 +74,10 @@ function run() {
     return true;
 }
 
-if(!run()){
+if (!run()) {
     console.error("exit with failure");
     process.exit(1);
 } else {
     console.log('done');
 }
+
